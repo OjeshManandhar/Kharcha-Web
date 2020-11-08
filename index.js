0a73e468cca942790f1ad236b75bf592b0d1cbf3
OjeshManandhar <ozes.manandhar@gmail.com>
2020-11-08 10:52:52 +0545

    Create a basic server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Index.js');
+const app = require('express')();
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Express Server</h1>');
+});
+
+app.listen(3000, () => console.log('Server started at port 3000'));
