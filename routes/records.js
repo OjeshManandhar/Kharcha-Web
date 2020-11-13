db7d1c46d0462ed37fda587a4049075f7f0c25aa
OjeshManandhar <ozes.manandhar@gmail.com>
2020-11-13 10:55:27 +0545

    Add edit.ejs in records

--- routes/records.js
+++ routes/records.js
@@ -1,29 +1,38 @@
 // packages
+const { render } = require('ejs');
 const express = require('express');
 
 // utils
 const { path } = require('./../utils/path');
 
+function _path(...args) {
+  return path('records', ...args);
+}
+
 const router = express.Router();
 
 router.get('/', function (req, res) {
-  res.render(path('records', 'index'), { title: 'Records' });
+  res.render(_path('index'), { title: 'Records' });
 });
 
 router.get('/add', (req, res) => {
-  res.render(path('records', 'add'), { title: 'Add Record' });
+  res.render(_path('add'), { title: 'Add Record' });
 });
 
 router.get('/list', (req, res) => {
-  res.render(path('records', 'list'), { title: 'List Records' });
+  res.render(_path('list'), { title: 'List Records' });
 });
 
 router.get('/filter', (req, res) => {
-  res.render(path('records', 'filter'), { title: 'Filter Records' });
+  res.render(_path('filter'), { title: 'Filter Records' });
+});
+
+router.get('/edit', (req, res) => {
+  res.render(_path('edit'), { title: 'Edit Record' });
 });
 
 router.get('/delete', (req, res) => {
-  res.render(path('records', 'delete'), { title: 'Delete Record' });
+  res.render(_path('delete'), { title: 'Delete Record' });
 });
 
 router.post('/add', (req, res) => {
@@ -34,6 +43,12 @@ router.post('/add', (req, res) => {
 
 router.post('/filter', (req, res) => {
   console.log('filter record:', req.body);
+
+  res.redirect('/records');
+});
+
+router.post('/edit', (req, res) => {
+  console.log('edit record:', req.body);
 
   res.redirect('/records');
 });
--- routes/tags.js
+++ routes/tags.js
@@ -4,30 +4,34 @@ const express = require('express');
 // utils
 const { path } = require('./../utils/path');
 
+function _path(...args) {
+  return path('tags', ...args);
+}
+
 const router = express.Router();
 
 router.get('/', function (req, res) {
-  res.render(path('tags', 'index'), { title: 'Tags' });
+  res.render(_path('index'), { title: 'Tags' });
 });
 
 router.get('/add', (req, res) => {
-  res.render(path('tags', 'add'), { title: 'Add Tag' });
+  res.render(_path('add'), { title: 'Add Tag' });
 });
 
 router.get('/list', (req, res) => {
-  res.render(path('tags', 'list'), { title: 'List Tags' });
-});
-
-router.get('/edit', (req, res) => {
-  res.render(path('tags', 'edit'), { title: 'Edit Tag' });
+  res.render(_path('list'), { title: 'List Tags' });
 });
 
 router.get('/search', (req, res) => {
-  res.render(path('tags', 'search'), { title: 'Search Tag' });
+  res.render(_path('search'), { title: 'Search Tag' });
+});
+
+router.get('/edit', (req, res) => {
+  res.render(_path('edit'), { title: 'Edit Tag' });
 });
 
 router.get('/delete', (req, res) => {
-  res.render(path('tags', 'delete'), { title: 'Delete Tag' });
+  res.render(_path('delete'), { title: 'Delete Tag' });
 });
 
 router.post('/add', (req, res) => {
@@ -36,14 +40,14 @@ router.post('/add', (req, res) => {
   res.redirect('/tags');
 });
 
-router.post('/edit', (req, res) => {
-  console.log('edit tag:', req.body);
+router.post('/search', (req, res) => {
+  console.log('search tag:', req.body);
 
   res.redirect('/tags');
 });
 
-router.post('/search', (req, res) => {
-  console.log('search tag:', req.body);
+router.post('/edit', (req, res) => {
+  console.log('edit tag:', req.body);
 
   res.redirect('/tags');
 });
