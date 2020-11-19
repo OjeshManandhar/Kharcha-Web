f20c34cb9c7e6ba159bbffef6062ff37e01a9ef3
OjeshManandhar <ozes.manandhar@gmail.com>
2020-11-19 16:54:50 +0545

    Update path util

--- utils/path.js
+++ utils/path.js
@@ -3,4 +3,4 @@ const p = require('path');
 const root = p.dirname(process.mainModule.filename);
 
 module.exports.path = (...args) => p.join(...args);
-module.exports.htmlPath = (...args) => p.join(root, ...args);
+module.exports.pathFromRoot = (...args) => p.join(root, ...args);
