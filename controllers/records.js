92345abd5a5f2077bd3707e516ef20bd4bae5935
OjeshManandhar <ozes.manandhar@gmail.com>
2020-11-30 10:58:14 +0545

    Filter records

--- controllers/records.js
+++ controllers/records.js
@@ -11,6 +11,19 @@ function _path(...args) {
   return path('records', ...args);
 }
 
+function queryGenerator(data) {
+  // Convert to boolean for comparing
+  if (!!(data.start && data.end)) {
+    return { [Op.and]: [{ [Op.gte]: data.start }, { [Op.lte]: data.end }] };
+  } else if (!!data.start) {
+    return { [Op.gte]: data.start };
+  } else if (!!data.end) {
+    return { [Op.lte]: data.end };
+  }
+
+  return undefined;
+}
+
 // GET
 module.exports.get = {
   index: (req, res) => {
@@ -132,9 +145,127 @@ module.exports.post = {
       });
   },
   filter: (req, res) => {
-    console.log('filter record:', req.body);
+    // gc = givenCriteria
+    const gc = {
+      id: {
+        start: req.body['id-start'],
+        end: req.body['id-end']
+      },
+      date: {
+        start: req.body['date-start'],
+        end: req.body['date-end']
+      },
+      amount: {
+        start: req.body['amount-start'],
+        end: req.body['amount-end']
+      },
+      type: req.body.type,
+      tagsType: req.body['tags-type'],
+      tags: [],
+      description: req.body.description,
+      criteria: req.body.criteria
+    };
 
-    res.redirect('/records');
+    req.body.tags
+      .split(',')
+      // Remove whitespaces
+      .map(tag => tag.trim())
+      // Filter out invalid tags
+      .filter(t => !(t.length < 3 || t.length > 20))
+      // Remove duplicates
+      .forEach(t => {
+        if (!gc.tags.find(x => x.toLowerCase() === t.toLowerCase()))
+          gc.tags.push(t);
+      });
+
+    // console.log('Given criteria:', gc);
+
+    let temp;
+    const query = [];
+
+    // id
+    temp = queryGenerator(gc.id);
+    if (temp) {
+      query.push({ id: temp });
+    }
+
+    // date
+    temp = queryGenerator(gc.date);
+    if (temp) {
+      query.push({ date: temp });
+    }
+
+    // amount
+    temp = queryGenerator(gc.amount);
+    if (temp) {
+      query.push({ amount: temp });
+    }
+
+    // type
+    if (gc.type !== 'any') {
+      query.push({ type: gc.type });
+    }
+
+    // tags
+    const tagQuery = {};
+    if (gc.tags.length > 0) {
+      if (gc.tagsType === 'any') {
+        tagQuery[Op.or] = gc.tags;
+      } else if (gc.tagsType === 'all') {
+        tagQuery[Op.and] = gc.tags;
+      }
+    }
+
+    // description
+    if (gc.description) {
+      query.push({ description: { [Op.like]: `%${gc.description}%` } });
+    }
+
+    // console.log('query:', query);
+    // console.log('where:', {
+    //   where: {
+    //     [gc.criteria === 'any' ? Op.or : Op.and]: query
+    //   },
+    //   include: {
+    //     model: 'tags',
+    //     where: { ...tagQuery }
+    //   }
+    // });
+
+    req.user
+      .getRecords({
+        where: {
+          [gc.criteria === 'any' ? Op.or : Op.and]: query
+        },
+        include: {
+          model: Tag,
+          where: tagQuery
+        }
+      })
+      .then(records => {
+        if (records.length > 0) {
+          records.forEach((r, index) => console.log(index, r.toJSON()));
+        } else {
+          console.log('records:', records);
+        }
+
+        // if (records.length > 0) {
+        //   res.redirect(
+        //     '/message?message=No records found&backLink=/records/filter'
+        //   );
+        // } else {
+        //   res.end();
+        // }
+
+        if (records.length > 0) {
+          res.end();
+        } else {
+          res.redirect(
+            '/message?message=No records found&backLink=/records/filter'
+          );
+        }
+      })
+      .catch(err => console.log('user.getRecord err:', err));
   },
   edit: (req, res) => {
     const { id, date, amount, type, tags, description } = req.body;
